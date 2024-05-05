6f01f1025060795401a7b779bbf0a07895bb4d88
Valerii <valeri.github@gmail.com>
2024-05-05 15:42:18 +0300

    Create config

---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-default:"local" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env:"HTTP_ADDRESS" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env:"HTTP_TIMEOUT" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env:"HTTP_IDLE_TIMEOUT" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable not set")
+	}
+
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("CONFIG_PATH does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatal("Cannot read config: %s", err)
+	}
+
+	return &cfg
+}
